d334acd107b1f543dc54b7209993f40c1b3a56ed
Aryankarma <110737632+Aryankarma@users.noreply.github.com>
2023-05-30 18:26:14 +0000

    initialising book store project

--- aryan/src/App.js
+++ aryan/src/App.js
@@ -1,18 +1,37 @@
-// import logo from './logo.svg';
 import './App.css';
 
-function App(){
- return  <h2> This is my first react component </h2>
-}
-export function Aryan(){
-  return (
-    <h5>This ismy second function</h5>
-  )
-} 
-export const Aryan2 = () => {
-  return (
-    <h1>Abhishek is teaching me react</h1>
-  )
+const BookList =()=>{
+  return <div>
+    <Book/>
+    <Book/>
+    <Book/>      
+  </div>
+};
+
+const Book =()=>{
+  return <div>
+    <Img/>
+    <Bookname/>
+    <Author/>
+  </div>
+};
+
+const Img =()=>{
+  return <div>
+    <img src="https://m.media-amazon.com/images/I/81f+YzrzDjS._AC_UL400_.jpg"/>    
+  </div>
+};
+
+const Bookname =()=>{
+  return <div>
+    <h2>A Christmas Carol</h2>
+  </div>
+};
+
+const Author =()=>{
+  return <div>
+    <h2>Charles Dickens</h2>
+    </div>
 }
 
-export default App;
+export default BookList;

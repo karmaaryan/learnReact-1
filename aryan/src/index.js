5159a6fab9356a5e7ac391982a3a527b2349039e
Aryan Karma <110737632+Aryankarma@users.noreply.github.com>
2023-08-02 18:50:23 +0000

    rebuild

--- aryan/src/index.js
+++ aryan/src/index.js
@@ -1,17 +1,49 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import BookList from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
+
+const Greeting = () => { return <div className='container'>
+        <Heading/>
+        <Box/>
+        <Box/>
+        <Box/>
+    </div>
+}
+
+{/* <img src='https://m.media-amazon.com/images/I/71YjqWYLt4L.jpg'/> */}
+
+const Heading =()=>{ return <div className='heading'>
+    <h1> Amazon Best Sellers </h1>
+</div>
+}
+const Box =() =>{ return <div className='box'>
+    <Img/>
+    <Name/>
+    <Author/>_
+</div>
+}
+const Img =()=>{ return <div className='imgCont'>
+    <img src='https://m.media-amazon.com/images/I/71YjqWYLt4L.jpg'/>
+</div>
+}
+const Name =()=>{ return <div className='name'>
+    <h3>World's Greatest Short Stories</h3>
+</div> 
+}
+
+const Author =()=>{ return <div className='author'>
+    <h6>By Collectible Edition</h6>
+</div>
+}
+
+
+
+
+
+
+
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BookList/>
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Greeting/>);

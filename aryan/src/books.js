66631097f29e936875f1a6456e20c1e1f2a3a170
Aryan Karma <110737632+Aryankarma@users.noreply.github.com>
2023-09-08 08:59:11 +0000

    proper list and map

--- aryan/src/books.js
+++ aryan/src/books.js
@@ -18,23 +18,23 @@ const book = [
 ]
 export default book;
 
-// export const firstBook = {
-//     img:"https://m.media-amazon.com/images/I/41r6F2LRf8L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
-//     name:"The Psychology Of Money",
-//     author:"Morgan Housel",
-//     id:1
-// }
+export const firstBook = {
+    img:"https://m.media-amazon.com/images/I/41r6F2LRf8L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+    name:"The Psychology Of Money",
+    author:"Morgan Housel",
+    id:1
+}
 
-// export const secondBook = {
-//     img:"https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
-//     name:"Atomic Habits",
-//     author:"James Clear",
-//     id:2,
-// }
+export const secondBook = {
+    img:"https://m.media-amazon.com/images/I/51-nXsSRfZL._SX328_BO1,204,203,200_.jpg",
+    name:"Atomic Habits",
+    author:"James Clear",
+    id:2,
+}
 
-// export const thirdbook = {
-//     img:"https://m.media-amazon.com/images/I/511vJPN7p5L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
-//     name:"The Subtle Art Of Not Giving A Fu*k",
-//     author:"Mark Manson",
-//     id:3,
-// }
+export const thirdbook = {
+    img:"https://m.media-amazon.com/images/I/511vJPN7p5L._SY264_BO1,204,203,200_QL40_FMwebp_.jpg",
+    name:"The Subtle Art Of Not Giving A Fu*k",
+    author:"Mark Manson",
+    id:3,
+}
--- aryan/src/index.js
+++ aryan/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import './index.css'
-// import {firstBook, secondBook, thirdbook} from './books'
-import books from "./books"
+import {firstBook, secondBook, thirdbook} from './books'
+import booksObj from "./books"
 
 const MainComponent=()=>{
     return <section className="main">
@@ -11,34 +11,62 @@ const MainComponent=()=>{
     </section>
 } 
 
-const mapping = books.map((input)=>{
-    return input;
-})
+// const mapping = books.map((input)=>{
+//     return input;
+// })
 
-console.log(books);
+// function BookList() {
+//     return <section className='booklist'>{books}</section>;
+// }
+
+
+// const return1 = ["aryan","karma","is","the","best","coder"];
+// this map returns the headings to the array values
+/*  const return2 = return1.map((input)=>{
+         return <h4>{input}</h4>
+    })
+    console.log(return2);
+*/
+
+// this map function returns the author names with perfect iteration int the objects;
+/*  const return2 = booksObj.map((input)=>{
+        console.log(input)
+        return <h2>{input.author}</h2>;
+    })
+*/
+// now we are creating a fucntion that would be returning the whole function
+// const returnBook = booksObj.map((input)=>{
+//     return <Book book={input}/>
+// })
+
+
 
 const Booklist=()=>{
     return <div className="booklist">
-        {/* <Book img={firstBook.img} author={firstBook.author} name={firstBook.name}/> */}
+        {/* working approach */}
+        {booksObj.map((input)=>{
+            return <Book book={input}/>
+        })}
+        {/* <Book img={firstBook.img} auBthor={firstBook.author} name={firstBook.name}/> */}
         {/* <Book img={secondBook.img} author={secondBook.author} name={secondBook.name}/> */}
         {/* <Book img={thirdbook.img} author={thirdbook.author} name={thirdbook.name}/> */}
-        <Book book={books[0]} />
-        <Book book={books[1]} />
-        <Book book={books[2]} />
+        {/* <section>{return1}</section> */}
+        {/* <section>{return2}</section> */}
+        {/* this is not working right now */}
+        {/* {returnBook(booksObj)} */}  
+        {/* <Book book={firstBook} />
+        <Book book={secondBook} />
+        <Book book={thirdbook} /> */}
     </div>
 }
-
 const Book=(props)=>{
     const {img, author, name, id} = props.book;
-    const {children} = props;
     return <div className="book">
         <img src={img}/>
         <h4>{name}</h4>
         <p>{author}</p>
-        {children}
     </div>
 }
-
 const Root = ReactDom.createRoot(document.getElementById("root"));
 
 Root.render(

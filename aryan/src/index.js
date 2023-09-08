350ee20fcba518c5eacdc710893785059216056a
Aryan Karma <110737632+Aryankarma@users.noreply.github.com>
2023-09-08 07:57:48 +0000

    fundamentals1

--- aryan/src/index.js
+++ aryan/src/index.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import ReactDom from 'react-dom/client';
 import './index.css'
+// import {firstBook, secondBook, thirdbook} from './books'
+import books from "./books"
 
 const MainComponent=()=>{
-    return <>
+    return <section className="main">
+        <h1>Amazon Best Sellers</h1>  
         <Booklist/>
-    </>
-}
+    </section>
+} 
+
+const mapping = books.map((input)=>{
+    return input;
+})
+
+console.log(books);
 
 const Booklist=()=>{
-    return <div>
-        <Book/>
-        <Book/>
-        <Book/>
+    return <div className="booklist">
+        {/* <Book img={firstBook.img} author={firstBook.author} name={firstBook.name}/> */}
+        {/* <Book img={secondBook.img} author={secondBook.author} name={secondBook.name}/> */}
+        {/* <Book img={thirdbook.img} author={thirdbook.author} name={thirdbook.name}/> */}
+        <Book book={books[0]} />
+        <Book book={books[1]} />
+        <Book book={books[2]} />
     </div>
 }
 
-const Book=()=>{
-    return <div>
-        <img src='../public/img/2.jpg'/>
+const Book=(props)=>{
+    const {img, author, name, id} = props.book;
+    const {children} = props;
+    return <div className="book">
+        <img src={img}/>
+        <h4>{name}</h4>
+        <p>{author}</p>
+        {children}
     </div>
 }
 

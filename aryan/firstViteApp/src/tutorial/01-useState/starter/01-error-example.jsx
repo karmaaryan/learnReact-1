808707b2489e5b6a583c3a378b99beb5d7953bba
Aryan Karma <110737632+Aryankarma@users.noreply.github.com>
2023-09-12 17:03:00 +0000

    useStateBasics

--- aryan/firstViteApp/src/tutorial/01-useState/starter/01-error-example.jsx
+++ aryan/firstViteApp/src/tutorial/01-useState/starter/01-error-example.jsx
@@ -1,5 +1,17 @@
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  
+  let count = 0;
+  
+  const increaseCount =()=>{
+    count++;
+    console.log(count);
+  }
+
+  return <>
+    <h2>useState error example</h2>
+    <h4>Count is : {count}</h4>
+    <button onClick={increaseCount}>Count++</button>
+  </>;
 };
 
 export default ErrorExample;
--- aryan/firstViteApp/src/tutorial/01-useState/starter/02-useState-basics.jsx
+++ aryan/firstViteApp/src/tutorial/01-useState/starter/02-useState-basics.jsx
@@ -1,5 +1,26 @@
+import { useState } from "react";
+
 const UseStateBasics = () => {
-  return <h2>useState basics</h2>;
+
+  let numberStart = 0;
+
+  const [numberValue, increaseCount]=useState(numberStart);
+  const [numberValue2, increaseCount2]=useState(numberStart);
+
+  const func=()=>{
+    increaseCount(numberValue+1);
+  }
+  const func2=()=>{
+    increaseCount2(numberValue2+1);
+  }
+  
+  return <>
+    <h2>useState basics</h2>;
+    <h4>Count 1 is : {numberValue}</h4>
+    <button className="btn" onClick={func}>Count++</button> 
+    <h4>Count 2 is : {numberValue2}</h4>
+    <button className="btn" onClick={func2}>Count++</button>
+  </> 
 };
 
 export default UseStateBasics;

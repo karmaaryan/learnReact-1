8cbf2757f3a00d0492afd802e9e08a9dc352fbb8
karmaaryan <146410371+karmaaryan@users.noreply.github.com>
2023-11-25 10:51:54 +0000

    redux setup

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Final from "./tutorial/09-context-api/final/context/Navbar"
-import Navbar from './tutorial/09-context-api/starter/components/navbar'
-
-export default function App(){
-  return (
-      <Navbar/>
-  );
-}
